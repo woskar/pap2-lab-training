ed02b2ea71518f081c94b14d49feecb98e6004b9
Oskar Weinfurtner <oskar.weinfurtner@icloud.com>
2016-12-03 20:16:40 +0100

    work done on 243

--- v243/v243.py
+++ v243/v243.py
@@ -1,6 +1,7 @@
 PAP2 V243: Thermisches Rauschen
 
 # Teil 1: Messwerte mit Fit plotten
+
 %matplotlib inline
 import matplotlib.pyplot as plt
 import numpy as np
@@ -19,7 +20,6 @@ def fit_func(f, V, W1, W2, n1, n2):
 
 #Startwerte p0 für die Fitparameter
 p0 = [1000, 1000, 50000, 5, 5]
-
 #Verstärkung V: 1000
 #untere Grenzfrequenz W1: 1000
 #obere Grenzfrequenz W2: 50000
@@ -31,7 +31,6 @@ popt, pcov = curve_fit(fit_func, f[17:-29],g[17:-29], p0)
 
 #Ermitteln des 1-sigma-Fehlers:
 sigma = np.sqrt(pcov[0,0])
-
 
 # Numerische Integration, aus nächster Aufgabe, für Plot vorgezogen
 import scipy.integrate as integrate
@@ -58,6 +57,13 @@ plt.savefig('Frequenzgangmessung.pdf', dpi=300, format='pdf')
 
 
 #Teil 2: Bestimmen der Boltzmannkonstante
+%matplotlib inline
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+
+plt.rcParams["figure.figsize"][0] = 8
+plt.rcParams["figure.figsize"][1] = 5
 
 #Messwerte aus dem Protokoll
 R=np.array([5e3, 10e3, 15e3, 20e3, 25e3, 30e3]) #Widerstände
@@ -65,7 +71,7 @@ U_aus=np.array([2.4093, 3.1198, 3.6971, 4.2011, 4.6534, 5.0728 ]) #Rauschspannun
 fehler_U_aus=np.array([0.00825, 0.0111, 0.0133, 0.0166, 0.0162, 0.0187]) #Standardabweichung in mV
 U_V=1.3803e-3 #Rauschspannung bei 0 Ohm
 
-#Bereinigen der Messwerte und Fehlerformel
+#Bereinigen der Messwerte und Aufstellen der Fehlerformel
 D=(U_aus**2)-(U_V**2)
 fehler_D=np.sqrt((2*U_aus*fehler_U_aus)**2+(2*U_V*7e-7)**2)
 
@@ -74,11 +80,11 @@ plt.errorbar(R, D, yerr=fehler_D, fmt='.', label='Messwerte mit Fehler')
 plt.axis([0, 3.2e4, 0, 25])
 
 #Fitten einer Gerade an die Daten:
-def linear(x, c):
-    return c*x
+def linear(x, c, t):
+    return c*x + t
 popt, pcov = curve_fit(linear, R, D)
 x = np.linspace(0,30000, 100) #neue x-Achse definieren, weil R "0" nicht enthält
-plt.plot(x, linear(popt[0],x), linestyle='-', label='Fitfunktion $f(x)=c*x$') #Fit plotten
+plt.plot(x, linear(x, *popt), linestyle='-', label='Fitfunktion $f(x)=c*x + t $') #Fit plotten
 stdfehler = np.sqrt(pcov[0,0])
 
 #Chi-quadrat-Summe berechnen
@@ -102,10 +108,18 @@ plt.legend(loc='best', borderpad=1, borderaxespad=1, shadow='true', fontsize='me
 #Plotten der berechneten Werte unter die Legende
 plt.rcParams["figure.figsize"][0] = 8
 plt.rcParams["figure.figsize"][1] = 5
-plt.text(1000, 17, '$c=$%.2e $\pm$ %.2e' %(popt[0], stdfehler ), fontsize=10)
-plt.text(1000, 15.5, '$\chi^{2}=$ %.1f' %(chisquare), fontsize=10)
-plt.text(1000, 14, '$\chi_{red}^{2}=$ %.1f ' %(chisquare_red), fontsize=10)
-plt.text(1000, 12.5, '$P=$ %.2f ' %(prob), fontsize=10)
+plt.text(1000, 16.5, '$c=$%.2e $\pm$ %.2e' %(popt[0], stdfehler ), fontsize=10)
+plt.text(1000, 15, '$\chi^{2}=$ %.1f' %(chisquare), fontsize=10)
+plt.text(1000, 13.5, '$\chi_{red}^{2}=$ %.1f ' %(chisquare_red), fontsize=10)
+plt.text(1000, 12, '$P=$ %.2f ' %(prob) + '%', fontsize=10)
+
+#Subplot mit Streudiagramm
+plt.axes([.55, .2, .25, .25])
+plt.xticks(())
+plt.yticks(())
+plt.title('Streudiagramm')
+plt.scatter(R, (linear(R, *popt)-D))
+plt.grid(True)
 
 #Plot abspeichern
-plt.savefig('Rauschspannung.pdf', dpi=300, format='pdf')
+plt.savefig('Rauschspannung_gut.pdf', dpi=300, format='pdf')
